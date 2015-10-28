6eaad49e571f0d08cf8a13386f20b0c619062b24
Georgios Koloventzos <sfagmenos@users.noreply.github.com>
2015-10-28 09:59:48 -0400

    more kb in the loop

--- localtests/sequential.sh
+++ localtests/sequential.sh
@@ -3,9 +3,8 @@
 hostname=`hostname`
 
 host=${hostname%%.*} 
-for i in 4 64 512 1024 2048; do   
+for i in 4 8 16 32 64 128 256 512 1024 2048; do   
    ./run_all.sh 0 0 40 0 "${i}k"
-   echo "fio_${i}k_rr_${host}.txt"
    mv time.txt "fio_${i}k_rr_${host}.txt"
    ./run_all.sh 0 0 0 40 "${i}k"
    mv time.txt "fio_${i}k_rw_${host}.txt"
